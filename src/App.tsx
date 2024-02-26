dffd99080d57569dba6c92e4a03c4f6833635292
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-02-26 17:21:21 +0100

    work on meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { Helmet } from 'react-helmet-async'
 import './App.css'
 
 function App() {
 
   return (
-    <main>
-      
-    </main>
+    <>
+      <Helmet>
+        <title>C.R.I Fluid Systems - Overview</title>
+        <meta name="title" content="C.R.I Fluid Systems - Overview" />
+        <meta name="description" content="Pumping trust worldwide" />
+      </Helmet>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Quizon</h1>
+        </header>
+      </div>
+    </>
   )
 }
 
