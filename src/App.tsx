d1d5c3f45329a2cf21acda43088a27dfc3714d68
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-02-26 19:00:26 +0100

    work on main

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import './App.css'
-import { Helmet, HelmetProvider} from 'react-helmet-async'
 import logo from "./assets/logo.png"
+import Img1 from "./assets/1.png"
+import Img2 from "./assets/2.png"
+import Img3 from "./assets/3.png"
 
 function App() {
 
-  const HelmetContext = {}
-
   return (
-    <HelmetProvider context={HelmetContext}>
-      <Helmet>
-        <title>C.R.I Fluid Systems - Overview</title>
-        <meta name="title" content="C.R.I Fluid Systems - Overview" />
-        <meta name="description" content="Pumping trust worldwide" />
-      </Helmet>
+    <>
         <header>
-          <img src={logo}/>
+          <img src={logo} alt="logo"/>
         </header>
-        <main></main>
+        <main>
+          <section className='section1'>
+            <div className='section1_div1'>
+              <img src={Img1} alt="Image 1" />
+            </div>
+            <div className="section1_div2">
+              <b>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</b>
+              <ul>
+                <li>
+                  C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.
+                </li>
+                <li>
+                  C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.
+                </li>
+              </ul>
+              <img src={Img2} alt="Image 2" />
+              <p>Government of India has awarded the <b>"National Energy Conservation Award 2018"</b>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+            </div>
+          </section>
+          <section className='section2'>
+            <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
+            <img src={Img3} alt="Image 3"/>
+          </section>
+        </main>
         <footer></footer>
-    </HelmetProvider>
+    </>
   )
 }
 
